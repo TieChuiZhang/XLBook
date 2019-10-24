c9280beb3aa12affb9d67185118d852bbb082574
tiechuichen <1015011319@qq.com>
2019-10-24 12:35:00 +0800

    缺少hxgCell.m文件 添加 time:20191024

--- XLBook/ALL/Classes/TabBar/TopBookList/View/TopBookOneBookHXJCell.h
+++ XLBook/ALL/Classes/TabBar/TopBookList/View/TopBookOneBookHXJCell.h
@@ -2,14 +2,13 @@
 //  TopBookOneBookHXJCell.h
 //  XLBook
 //
-//  Created by Lee on 2019/10/23.
+//  Created by Lee on 2019/10/24.
 //  Copyright © 2019 Lee. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "TopBookOneBookDModel.h"
 NS_ASSUME_NONNULL_BEGIN
-
 @interface TopBookOneBookHXJCell : UITableViewCell
 + (instancetype)xlTopBookOneBookHXJCellWithTableView:(UITableView *)tableView IndexPathRow:(NSInteger)row;
 - (void)setXLBookOneBookDHXJModelCellValue:(TopBookOneBookDModel *)xlBookOneBookDMLModel ArrayWithHXGDataArray:(NSArray *)dataArray;
