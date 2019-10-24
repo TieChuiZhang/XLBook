8553210c94cf49f91ea66d7a5b16e79798c41a42
tiechuilee <1015011319@qq.com>
2019-10-24 20:53:57 +0800

    删除

---
+++ XLBook/ALL/Classes/TabBar/ReadView/Controller/ZGiReader/Controller/ZGiReaderTopAndBottomBarController.h
@@ -0,0 +1,13 @@
+//
+//  ZGiReaderTopAndBottomBarController.h
+//  ZGiReader
+//
+//  Created by 杨振国 on 2018/8/3.
+//  Copyright © 2018年 aikutv. All rights reserved.
+//
+
+#import "XLBookBaseController.h"
+
+@interface ZGiReaderTopAndBottomBarController : XLBookBaseController
+- (void)hiddenOrShowZGiReaderTopAndBottomBar;
+@end
